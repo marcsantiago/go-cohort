ff4d6f451668bef26a1bfb85a02053078d68028f
Marc Santiago <marcanthonysanti@gmail.com>
2020-07-20 22:44:18 -0400

    Cohort: Adding an unknown type

--- split_type.go
+++ split_type.go
@@ -1,6 +1,10 @@
 package cohorts
 
-type SplitType uint8
+type SplitType int8
+
+// returns -1 because when using the value in mod math the split will always be 0 and therefore fall into group A without a B group
+// this in turn means the split is not running, but also nothing is panicking or breaking.
+const SplitTypeUnknown = SplitType(-1)
 
 const (
 	SplitCohortAB = iota + 2
